5c688c2dada4236d377149e3290a84b1f8791c4a
pavel-cpp <toruchan174@gmail.com>
2024-03-27 21:30:47 +0300

    add render method

--- transport-catalogue/map_renderer.cpp
+++ transport-catalogue/map_renderer.cpp
@@ -4,32 +4,34 @@
 #include <utility>
 
 namespace renderer {
-    
-    MapRenderer::MapRenderer(Settings settings) : settings_(settings){}
+
+    MapRenderer::MapRenderer(Settings settings) :
+        settings_(settings) {
+    }
 
     void MapRenderer::AddBus(const Bus &bus) {
         buses_.insert(bus);
     }
 
     void MapRenderer::CalculateZoomCoef() {
-        geo::Coordinates min_coords;
-        geo::Coordinates max_coords;
-        for(const Bus& bus: buses_){
+        geo::Coordinates min_coords{};
+        geo::Coordinates max_coords{};
+        for (const Bus &bus: buses_) {
 
             const auto [min_lat, max_lat] = std::minmax_element(
-                bus.route_.begin(),
-                bus.route_.end(),
-                [](const Stop* lhs, const Stop* rhs) {
-                    return lhs->position_.lat < rhs->position_.lat;
-                }
-            );
+                    bus.route_.begin(),
+                    bus.route_.end(),
+                    [](const Stop *lhs, const Stop *rhs) {
+                        return lhs->position_.lat < rhs->position_.lat;
+                    }
+                    );
             const auto [min_lng, max_lng] = std::minmax_element(
-                bus.route_.begin(),
-                bus.route_.end(),
-                [](const Stop* lhs, const Stop* rhs) {
-                    return lhs->position_.lng < rhs->position_.lng;
-                }
-            );
+                    bus.route_.begin(),
+                    bus.route_.end(),
+                    [](const Stop *lhs, const Stop *rhs) {
+                        return lhs->position_.lng < rhs->position_.lng;
+                    }
+                    );
 
             min_coords.lat = std::min(min_coords.lat, (*min_lat)->position_.lat);
             min_coords.lng = std::min(min_coords.lng, (*min_lng)->position_.lng);
@@ -37,29 +39,60 @@ namespace renderer {
             max_coords.lat = std::max(max_coords.lat, (*max_lat)->position_.lat);
             max_coords.lng = std::max(max_coords.lng, (*max_lng)->position_.lng);
         }
+
         min_lng_ = min_coords.lng;
         max_lat_ = max_coords.lat;
-        
-        if(max_coords.lng - min_coords.lng && max_coords.lat - min_coords.lat){
-            double width_zoom_coef = (width_ - 2 * padding_) / (max_coords.lng - min_coords.lng);
-            double height_zoom_coef = (height_ - 2 * padding_) / (max_coords.lat - min_coords.lat);
+
+        if (max_coords.lng - min_coords.lng && max_coords.lat - min_coords.lat) {
+            double width_zoom_coef = (settings_.width_ - 2 * settings_.padding_) / (max_coords.lng - min_coords.lng);
+            double height_zoom_coef = (settings_.height_ - 2 * settings_.padding_) / (max_coords.lat - min_coords.lat);
             zoom_coef_ = std::min(width_zoom_coef, height_zoom_coef);
         }
-        
     }
 
-    std::pair<double, double> MapRenderer::CoordinatesToPoint(geo::Coordinates coords) const {
+    svg::Point MapRenderer::CoordinatesToPoint(geo::Coordinates coords) const {
         return {
-            (coords.lng - min_lng_) * zoom_coef_ + padding_,
-            (max_lat_ - coords.lat) * zoom_coef_ + padding_
+                (coords.lng - min_lng_) * zoom_coef_ + settings_.padding_,
+                (max_lat_ - coords.lat) * zoom_coef_ + settings_.padding_
         };
     }
 
     void MapRenderer::Render(std::ostream &out) {
         CalculateZoomCoef();
 
-        
-            
+        svg::Document map_render;
+
+        size_t color_number = 0;
+        for (const Bus &bus: buses_) {
+            svg::Polyline route_line;
+            svg::Circle stop_point;
+            stop_point.SetRadius(settings_.stop_radius_).SetFillColor(settings_.color_palette_[color_number]);
+            svg::Text stop_label;
+            stop_label.SetFontSize(settings_.stop_label_font_size_).SetOffset(settings_.stop_label_offset_);
+            for (const Stop *stop: bus.route_) {
+                auto position = CoordinatesToPoint(stop->position_);
+                route_line.AddPoint(position);
+                map_render.Add(stop_point.SetCenter(position));
+                map_render.Add(stop_label.SetPosition(position).SetData(stop->name_));
+            }
+            svg::Text bus_label;
+            bus_label.SetData(bus.name_)
+                    .SetFillColor(settings_.color_palette_[color_number])
+                    .SetOffset(settings_.bus_label_offset_)
+                    .SetFontSize(settings_.bus_label_font_size_);
+            map_render.Add(bus_label.SetPosition(CoordinatesToPoint(bus.route_.front()->position_)));
+            map_render.Add(bus_label.SetPosition(CoordinatesToPoint(bus.route_.back()->position_)));
+            map_render.Add(
+                    route_line
+                    .SetFillColor(settings_.color_palette_[color_number])
+                    .SetStrokeWidth(settings_.width_)
+                    .SetStrokeLineCap(svg::StrokeLineCap::ROUND)
+                    .SetStrokeLineJoin(svg::StrokeLineJoin::ROUND)
+                    );
+        }
+
+        map_render.Render(out);
+
     }
 
 } // renderer
--- transport-catalogue/map_renderer.h
+++ transport-catalogue/map_renderer.h
@@ -10,52 +10,26 @@
 
 namespace renderer {
 
-    // {
-    //   "width": 1200.0,
-    //   "height": 1200.0,
-
-    //   "padding": 50.0,
-
-    //   "line_width": 14.0,
-    //   "stop_radius": 5.0,
-
-    //   "bus_label_font_size": 20,
-    //   "bus_label_offset": [7.0, 15.0],
-
-    //   "stop_label_font_size": 20,
-    //   "stop_label_offset": [7.0, -3.0],
-
-    //   "underlayer_color": [255, 255, 255, 0.85],
-    //   "underlayer_width": 3.0,
-
-    //   "color_palette": [
-    //     "green",
-    //     [255, 160, 0],
-    //     "red"
-    //   ]
-    // }
-
     struct Settings {
         double width_{};
         double height_{};
-    
+
         double padding_{};
 
         double line_width_{};
         double stop_radius_{};
 
         size_t bus_label_font_size_{};
-        std::pair<double, double> bus_label_offset_{};
+        svg::Point bus_label_offset_{};
 
         size_t stop_label_font_size_{};
-        std::pair<double, double> stop_label_offset_{};
+        svg::Point stop_label_offset_{};
 
-        svg::Rgba underlayer_color_{};
+        svg::Color underlayer_color_{};
         double underlayer_width_{};
 
-        std::array<svg::Color, 3> color_palette_{};
-    }
-    
+        std::vector<svg::Color> color_palette_{};
+    };
 
     class MapRenderer final {
     public:
@@ -63,14 +37,13 @@ namespace renderer {
 
         explicit MapRenderer(Settings settings);
 
-        void AddBus(const Bus& bus);
+        void AddBus(const Bus &bus);
 
-        void Render(std::ostream& out);
+        void Render(std::ostream &out);
 
     private:
+        svg::Point CoordinatesToPoint(geo::Coordinates coords) const;
 
-        std::pair<double, double> CoordinatesToPoint(geo::Coordinates coords) const;
-        
         void CalculateZoomCoef();
 
         Settings settings_;
