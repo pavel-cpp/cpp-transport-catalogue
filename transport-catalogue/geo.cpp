9f5912874ebbad038531feaf3a01daba3f4ae968
Pavel Remdenok <toruchan174@gmail.com>
2024-03-31 16:48:48 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -9,6 +9,6 @@ namespace geo {
         const double dr = M_PI / 180.0;
         return acos(sin(from.lat * dr) * sin(to.lat * dr)
                     + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-               * 6371000;
+               * EARTH_RADIUS;
     }
 } // namespace geo
