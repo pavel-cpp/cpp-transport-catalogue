b5fea489f5bc11d33a9c732dc6ae0decbca4deeb
pavel-cpp <toruchan174@gmail.com>
2024-05-29 22:53:51 +0300

    Fix after review

--- transport-catalogue/transport_router.cpp
+++ transport-catalogue/transport_router.cpp
@@ -3,65 +3,15 @@
 #include <memory>
 
 TransportRouter::TransportRouter(
-        uint8_t bus_wait_time,
-        double bus_velocity,
+        const TransportRouterSettings &settings,
         const TransportCatalogue &catalogue
-        ) : bus_wait_time_(bus_wait_time),
-            bus_velocity_(bus_velocity)
-{
-    const auto &all_stops = catalogue.GetAllSortedStops();
-    const auto &all_buses = catalogue.GetAllSortedBuses();
-    graph::DirectedWeightedGraph<double> stops_graph(all_stops.size() * 2);
-    std::map<std::string, graph::VertexId> stop_ids;
+        ) : settings_(settings) {
+
+    graph::DirectedWeightedGraph<double> stops_graph(catalogue.GetAllSortedStops().size() * 2);
     graph::VertexId vertex_id = 0;
 
-    for (const auto &[stop_name, stop_info]: all_stops) {
-        stop_ids[stop_info->name_] = vertex_id;
-        stops_graph.AddEdge({
-                stop_info->name_,
-                0,
-                vertex_id,
-                ++vertex_id,
-                static_cast<double>(bus_wait_time_)
-        });
-        ++vertex_id;
-    }
-    stop_ids_ = std::move(stop_ids);
-
-    for_each(
-            all_buses.begin(),
-            all_buses.end(),
-            [&stops_graph, this, &catalogue](const auto &item) {
-                const Bus *bus_info = item.second;
-                const auto &stops = bus_info->route_;
-                size_t stops_count = stops.size();
-                for (size_t i = 0; i < stops_count; ++i) {
-                    for (size_t j = i + 1; j < stops_count; ++j) {
-                        const Stop *stop_from = stops[i];
-                        const Stop *stop_to = stops[j];
-                        size_t dist_sum = 0;
-                        size_t dist_sum_inverse = 0;
-                        for (size_t k = i + 1; k <= j; ++k) {
-                            dist_sum += catalogue.Distance(stops[k - 1], stops[k]);
-                            dist_sum_inverse += catalogue.Distance(stops[k], stops[k - 1]);
-                        }
-                        stops_graph.AddEdge({bus_info->name_,
-                                             j - i,
-                                             stop_ids_.at(stop_from->name_) + 1,
-                                             stop_ids_.at(stop_to->name_),
-                                             static_cast<double>(dist_sum) / (bus_velocity_ * (100.0 / 6.0))});
-
-                        if (!bus_info->is_roundtrip_) {
-                            stops_graph.AddEdge({bus_info->name_,
-                                                 j - i,
-                                                 stop_ids_.at(stop_to->name_) + 1,
-                                                 stop_ids_.at(stop_from->name_),
-                                                 static_cast<double>(dist_sum_inverse) / (
-                                                     bus_velocity_ * (100.0 / 6.0))});
-                        }
-                    }
-                }
-            });
+    FillGraphStops(catalogue, vertex_id, stops_graph);
+    FillGraphBuses(catalogue, vertex_id, stops_graph);
 
     graph_ = std::move(stops_graph);
     router_ = std::make_unique<graph::Router<double>>(graph_);
@@ -77,4 +27,59 @@ TransportRouter::FindRoute(
 
 TransportRouter::Graph TransportRouter::GetGraph() const {
     return graph_;
+}
+
+void TransportRouter::FillGraphStops(
+        const TransportCatalogue &catalogue,
+        graph::VertexId &vertex_id,
+        graph::DirectedWeightedGraph<double> &stops_graph
+        ) {
+    for (const auto &[stop_name, stop_info]: catalogue.GetAllSortedStops()) {
+        stop_ids_[stop_info->name_] = vertex_id;
+        stops_graph.AddEdge({
+                stop_info->name_,
+                0,
+                vertex_id,
+                ++vertex_id,
+                static_cast<double>(settings_.bus_wait_time)
+        });
+        ++vertex_id;
+    }
+}
+
+void TransportRouter::FillGraphBuses(
+        const TransportCatalogue &catalogue,
+        graph::VertexId &vertex_id,
+        graph::DirectedWeightedGraph<double> &stops_graph
+        ) {
+    for (const auto &[bus_name, bus_info]: catalogue.GetAllSortedBuses()) {
+        const auto &stops = bus_info->route_;
+        size_t stops_count = stops.size();
+        for (size_t i = 0; i < stops_count; ++i) {
+            for (size_t j = i + 1; j < stops_count; ++j) {
+                const Stop *stop_from = stops[i];
+                const Stop *stop_to = stops[j];
+                size_t dist_sum = 0;
+                size_t dist_sum_inverse = 0;
+                for (size_t k = i + 1; k <= j; ++k) {
+                    dist_sum += catalogue.Distance(stops[k - 1], stops[k]);
+                    dist_sum_inverse += catalogue.Distance(stops[k], stops[k - 1]);
+                }
+                stops_graph.AddEdge({bus_info->name_,
+                                     j - i,
+                                     stop_ids_.at(stop_from->name_) + 1,
+                                     stop_ids_.at(stop_to->name_),
+                                     static_cast<double>(dist_sum) / (settings_.bus_velocity * (100.0 / 6.0))});
+
+                if (!bus_info->is_roundtrip_) {
+                    stops_graph.AddEdge({bus_info->name_,
+                                         j - i,
+                                         stop_ids_.at(stop_to->name_) + 1,
+                                         stop_ids_.at(stop_from->name_),
+                                         static_cast<double>(dist_sum_inverse) / (
+                                             settings_.bus_velocity * (100.0 / 6.0))});
+                }
+            }
+        }
+    }
 }
--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -7,55 +7,67 @@
 
 #include "transport_catalogue.h"
 
+struct TransportRouterSettings {
+    uint8_t bus_wait_time;
+    double bus_velocity;
+};
+
 class TransportRouter final {
 public:
-
     using RouteInfo = graph::Router<double>::RouteInfo;
     using Graph = graph::DirectedWeightedGraph<double>;
 
-    explicit TransportRouter(uint8_t bus_wait_time, double bus_velocity, const TransportCatalogue& catalogue);
+    explicit TransportRouter(const TransportRouterSettings& settings, const TransportCatalogue &catalogue);
 
     [[nodiscard]] std::optional<RouteInfo> FindRoute(std::string_view stop_from, std::string_view stop_to) const;
 
+    // Испольуется для составления отчета и чтобы не городить optional пар
     [[nodiscard]] Graph GetGraph() const;
 
 private:
+    void FillGraphStops(const TransportCatalogue &catalogue,
+                        graph::VertexId &vertex_id,
+                        graph::DirectedWeightedGraph<double> &stops_graph);
 
-    uint8_t bus_wait_time_ {0};
-    double bus_velocity_ {0.0};
+    void FillGraphBuses(const TransportCatalogue &catalogue,
+                        graph::VertexId &vertex_id,
+                        graph::DirectedWeightedGraph<double> &stops_graph);
 
-    graph::DirectedWeightedGraph<double> graph_ {};
-    std::map<std::string, graph::VertexId> stop_ids_ {};
-    std::unique_ptr<graph::Router<double>> router_ {};
+    TransportRouterSettings settings_{};
+
+    graph::DirectedWeightedGraph<double> graph_{};
+    std::map<std::string, graph::VertexId> stop_ids_{};
+    std::unique_ptr<graph::Router<double>> router_{};
 
 };
 
 class TransportRouterBuilder final {
 public:
-
     TransportRouterBuilder() = delete;
-    explicit TransportRouterBuilder(const TransportCatalogue& catalogue) : catalogue_(catalogue) {}
-    TransportRouterBuilder(const TransportRouterBuilder&) = default;
-    TransportRouterBuilder(TransportRouterBuilder&&) = default;
 
-    TransportRouterBuilder& SetBusWaitTime(uint8_t value) noexcept {
-        bus_wait_time_ = value;
+    explicit TransportRouterBuilder(const TransportCatalogue &catalogue) :
+        catalogue_(catalogue) {
+    }
+
+    TransportRouterBuilder(const TransportRouterBuilder &) = default;
+    TransportRouterBuilder(TransportRouterBuilder &&) = default;
+
+    TransportRouterBuilder &SetBusWaitTime(uint8_t value) noexcept {
+        settings_.bus_wait_time = value;
         return *this;
     }
 
-    TransportRouterBuilder& SetBusVelocity(uint8_t value) noexcept {
-        bus_velocity_ = value;
+    TransportRouterBuilder &SetBusVelocity(uint8_t value) noexcept {
+        settings_.bus_velocity = value;
         return *this;
     }
 
     TransportRouter Build() const noexcept {
-        return TransportRouter {bus_wait_time_, bus_velocity_, catalogue_};
+        return TransportRouter{settings_, catalogue_};
     }
 
 private:
-
-    const TransportCatalogue& catalogue_ {};
-    uint8_t bus_wait_time_ {0};
-    double bus_velocity_ {0.0};
+    const TransportCatalogue &catalogue_{};
+    TransportRouterSettings settings_{};
 
 };
